22a955d4c0928e053cd813381f4dfb23329dd6bd
Radhika <radhikanaurvi93@gmail.com>
2021-11-12 00:49:23 +0530

    portfolio commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+  
+      if($(window).scrollTop() > 0){
+        $('.top').show();
+      }else{
+        $('.top').hide();
+      }
+  
+    });
+  
+    // smooth scrolling 
+  
+    $('a[href*="#"]').on('click',function(e){
+  
+      e.preventDefault();
+  
+      $('html, body').animate({
+  
+        scrollTop : $($(this).attr('href')).offset().top,
+  
+      },
+        500, 
+        'linear'
+      );
+  
+    });
+
+    function sendemail(name,email,message){
+        Email.send({
+            Host : "smtp.yourisp.com",
+            Username : "username",
+            Password : "password",
+            To : 'them@website.com',
+            From : "you@isp.com",
+            Subject : "This is the subject",
+            Body : "And this is the body"
+        }).then(
+          message => alert(message)
+        );
+    }
+  
+  });
